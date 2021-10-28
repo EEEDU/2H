f67986e2e4d304f02349ef777f7a7f62a9e28ef7
EEEDU <edu7guerrero@gmail.com>
2021-10-28 23:51:17 +0200

    SGE B2R0

--- SGE/B2R0/Ejercicio12.py
+++ SGE/B2R0/Ejercicio12.py
@@ -1,19 +1,31 @@
 class cuenta:
-    def __init__(self, titular, cantidad):
+    def __init__(self, titular, *cantidad):
         self.titular = titular
         self.cantidad = cantidad
+    
+    def get_titular(self):
+        return self.titular
+
+    def set_titular(self, tit):
+        self.titular = tit
+
+    def get_cantiad(self):
+        return self.cantidad
+
+    def set_cantidad(self, can):
+        self.cantidad = can
 
     def ingresar (self, can):
         if can > 0:
-            self.cantidad += can
-            print("Ahora la cuenta de ", self.titular, " tiene ", self.cantidad, " €")
+            self.set_cantidad(self.get_cantiad() + can)
+            print("Ahora la cuenta de ", self.get_titular(), " tiene ", self.get_cantiad(), " €")
         else:
             print("No se puede ingresar una cantidad negativa")
 
     def retirar (self, can):
-        if self.cantidad > can:
-            self.cantidad -= can
-            print("Ahora la cuenta de ", self.titular, " tiene ", self.cantidad, " €")
+        if self.get_cantidad() > can:
+            self.set_cantidad(self.get_cantiad() - can)
+            print("Ahora la cuenta de ", self.get_titular(), " tiene ", self.get_cantiad(), " €")
         else:
             print("Lo siento no tienes tanto dinero")
 
