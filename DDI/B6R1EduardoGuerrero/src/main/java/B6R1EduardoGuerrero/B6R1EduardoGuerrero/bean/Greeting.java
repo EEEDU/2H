f5217b579d1371e38674a7db9e4af0a5d2890360
Eduardo Guerrero <55740725+EEEDU@users.noreply.github.com>
2022-02-07 09:34:25 +0100

    Add files via upload

---
+++ DDI/B6R1EduardoGuerrero/src/main/java/B6R1EduardoGuerrero/B6R1EduardoGuerrero/bean/Greeting.java
@@ -0,0 +1,29 @@
+package B6R1EduardoGuerrero.B6R1EduardoGuerrero.bean;
+
+public class Greeting {
+
+    private String player;
+    private String message;
+
+    public Greeting(String player, String message) {
+        this.player = player;
+        this.message = message;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
