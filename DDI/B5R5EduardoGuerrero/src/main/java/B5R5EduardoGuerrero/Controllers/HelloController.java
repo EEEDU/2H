06536a732e9c07520bc54613c94e62ab5d6ade1c
Eduardo Guerrero <55740725+EEEDU@users.noreply.github.com>
2022-02-21 22:30:03 +0100

    Add files via upload

---
+++ DDI/B5R5EduardoGuerrero/src/main/java/B5R5EduardoGuerrero/Controllers/HelloController.java
@@ -0,0 +1,17 @@
+package B5R5EduardoGuerrero.Controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+
+        return "index";
+
+    }
+
+}
---
+++ DDI/B5R5EduardoGuerrero/src/test/java/B5R5EduardoGuerrero/B5R5EduardoGuerrero/B5R5EduardoGuerreroApplicationTests.java
@@ -0,0 +1,13 @@
+package B5R5EduardoGuerrero.B5R5EduardoGuerrero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B5R5EduardoGuerreroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
