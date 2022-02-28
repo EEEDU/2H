d51b0a9b4085ce501735cc0ab3d920f48e6ecb69
Eduardo Guerrero <55740725+EEEDU@users.noreply.github.com>
2022-02-28 22:27:20 +0100

    Add files via upload

---
+++ DDI/B5R8EduardoGuerrero/src/main/java/B5R8EduardoGuerrero/B5R8EduardoGuerrero/B5R8EduardoGuerreroApplication.java
@@ -0,0 +1,13 @@
+package B5R8EduardoGuerrero.B5R8EduardoGuerrero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B5R8EduardoGuerreroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B5R8EduardoGuerreroApplication.class, args);
+	}
+
+}
