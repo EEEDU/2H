14fec74578c39f45e2ca140602a2b2bacf837b38
Eduardo Guerrero <55740725+EEEDU@users.noreply.github.com>
2022-02-28 18:08:34 +0100

    Add files via upload

---
+++ DDI/B6R4EduardoGuerrero/src/test/java/B6R4EduardoGuerrero/B6R4EduardoGuerrero/B6R4EduardoGuerreroApplicationTests.java
@@ -0,0 +1,13 @@
+package B6R4EduardoGuerrero.B6R4EduardoGuerrero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B6R4EduardoGuerreroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
