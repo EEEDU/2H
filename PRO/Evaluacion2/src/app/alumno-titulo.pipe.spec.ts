558b43b80ea5d7bfea104f887ea4dbcec07ad83e
Eduardo Guerrero <55740725+EEEDU@users.noreply.github.com>
2022-03-08 18:23:26 +0100

    Delete PRO/Evaluacion2 directory

--- PRO/Evaluacion2/src/app/alumno-titulo.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AlumnoTituloPipe } from './alumno-titulo.pipe';
-
-describe('AlumnoTituloPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AlumnoTituloPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
